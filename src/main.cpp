1c1fc22471016c59dbe93d54fd31b19f614c45a7
mastrogippo <gipmad@gmail.com>
2019-08-07 13:07:38 +0200

    fixed license

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
-# This STM32F103 firmware interfaces an ATM90E3x with Atmel's GUI program
-# Copyright (C) 2019 Mastro Gippo
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program.  If not, see <http://www.gnu.org/licenses/>.
+// This STM32F103 firmware interfaces an ATM90E3x with Atmel's GUI program
+// Copyright (C) 2019 Mastro Gippo
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "mbed.h"
 #include "main.h"
